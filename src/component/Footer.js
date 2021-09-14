b33f9d633de5a979f242e0cc5b8b3d768569241e
AmerAbuZaiton123 <amerabuzaiton@outlook.com>
2021-09-15 01:03:04 +0300

    class 2

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,13 +1,13 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 
-class Footer extends Component{
-    render()
-    {
+ class Footer extends Component {
+    render() {
         return (
             <div>
-                <p>All Rights Reserved Amer 2021 © </p>    
+            <p> All Rights Reserved AmerAbuZaiton © </p>    
             </div>
         )
     }
 }
+
 export default Footer
